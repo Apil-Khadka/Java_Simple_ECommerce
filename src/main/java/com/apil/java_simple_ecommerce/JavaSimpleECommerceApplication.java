3299f443246ee06e60dd2afd89cd9c537dff9414
Apil Khadka <apilkhadka2020@gmail.com>
2025-04-16 08:46:06 +0545

    Initial commit

---
+++ src/main/java/com/apil/java_simple_ecommerce/JavaSimpleECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.apil.java_simple_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSimpleECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSimpleECommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apil/java_simple_ecommerce/JavaSimpleECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apil.java_simple_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSimpleECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
